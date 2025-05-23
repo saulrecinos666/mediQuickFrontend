4af1c7ff66d21de1c9dc13e9651af841ff7bf55f
Choster900 <16adonaysergio@gmail.com>
2025-05-23 13:45:59 -0600

    Implements basic click listeners for navigation drawer items
    - Adds click listeners to the menu button, notification button, and navigation view items in `HomeActivity.java`.
    - Displays a `Toast` message indicating which item was clicked.
    - Currently, only "Cerrar sesión" (Logout) has a specific action: finishing the activity. Other items show a generic "Clicked [item name]" toast.

--- app/src/main/java/com/example/mediquick/ui/activities/HomeActivity.java
+++ app/src/main/java/com/example/mediquick/ui/activities/HomeActivity.java
@@ -16,6 +16,7 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
